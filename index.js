80cd8f65767330f9f24e1407f5bd72a13abab73e
NycGio <giorgialvarez@gmail.com>
2021-09-04 12:26:35 -0400

    Added elements to control the timer and css tweaks

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 class Timer {
-    constructor(msg){
-        
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+
+
+    start(){
+        console.log('Time to start time')
     }
 }
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput,startButton,pauseButton);
